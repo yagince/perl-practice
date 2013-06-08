effc7f2e483d26fb155f406b06d5d1ac04102300
yagince <straitwalk@gmail.com>
2013-06-08 11:54:48 +0900

    string

--- string.pl
+++ string.pl
@@ -4,4 +4,18 @@ print 'hoge\nfoo' . "\n"; # \nは改行と認識されない
 
 print "hoge\nfoo" . "\n";
 
-print "aaa" x 5; # aaaが５回
+print "aaa" x 5 . "\n"; # aaaが５回
+
+$hoge = 100;
+print "$hoge hogehoge" . "\n";
+# print "$hogehogehoge" . "\n"; #これだと$hogehogehogeという変数と解釈されるっぽい？？
+print "${hoge}hogehoge" . "\n"; #これならOK
+print "${ 1 + 1 }hogehoge" . "\n"; #式はうみこめないのか？？
+
+
+# if ("hoge" eq "hoge") {
+if ("hoge" == "hoge") {
+  print "same!!!\n";
+} else {
+  print "not....same.....orz\n";
+}
