1d67c017001c4d687c365688c5817bc65172d8f2
yagince <straitwalk@gmail.com>
2013-06-27 23:00:39 +0900

    クラス変数

---
+++ oop/class_variable.pl
@@ -0,0 +1,19 @@
+use 5.012;
+
+{ package Hoge;
+  our %NAMES;
+  sub new {
+    my $class = shift;
+    my $name = shift;
+    my $self = bless \$name, $class;
+    $NAMES{$self} = $self;
+  }
+  sub all_names { map { ref($_)." named ".${$_} } values %NAMES }
+}
+
+my $hoge1 = Hoge->new("hoge1");
+my $hoge2 = Hoge->new("hoge2");
+my $hoge3 = Hoge->new("hoge3");
+
+my @names = $hoge1->all_names;
+say "@names";
