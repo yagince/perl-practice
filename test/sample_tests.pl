b98801dfd5ffd193a5c0e4f66c04a17aedeaedcc
yagince <straitwalk@gmail.com>
2013-06-30 22:31:41 +0900

    test sample

---
+++ test/sample_tests.pl
@@ -0,0 +1,9 @@
+use 5.012;
+
+use Test::More tests => 4;
+
+ok(1, '1 is true');
+is(2 + 2, 4, 'the sum is 4');
+is(2*3, 5, 'the product is 5'); # not ok
+isnt(2 ** 3, 6, "the result is not 6");
+like('Alpaca Book', qr/alpaca/i, 'I found an alpaca!');
