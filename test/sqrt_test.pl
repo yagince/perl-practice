5d600a3062e9df7119d6921e2c7b762d3d84b243
yagince <straitwalk@gmail.com>
2013-06-30 22:43:14 +0900

    sqrt test

---
+++ test/sqrt_test.pl
@@ -0,0 +1,34 @@
+use 5.012;
+
+use Test::More tests => 9;
+
+sub test_sub { '10000' }
+
+is($@, '', '$@ is not set at start');
+
+{
+  my $n = -1;
+  eval{ sqrt($n) };
+  ok($@, 'sqrt(-1)');
+}
+
+eval{ sqrt(1) };
+is($@, '', 'sqrt(1)');
+
+{
+  my $n = -100;
+  eval{ sqrt($n) };
+  ok($@, 'sqrt(-100)');
+}
+
+is(sqrt(test_sub()), 100, "string value works in sqrt");
+
+eval{ sqrt(undef) };
+is($@, '', "sqrt(undef)");
+
+is(sqrt, 0, "sqrt works on $_(undefined) by default");
+
+my $_ = 100;
+is(sqrt, 10, "sqrt works on $_ by default");
+
+is(sqrt(10**100), 10**50, "ok");
